251c502aff079df54fb2a760b91c4e8fe0cfeb6f
rositadiazgalaz <rositadiazgalaz@gmail.com>
2019-04-08 11:54:54 -0400

    terminando decode en chiperjs

--- src/cipher.js
+++ src/cipher.js
@@ -46,12 +46,25 @@ window.cipher = {
   		textASCII_decode=textoD.charCodeAt(i);
   		// caracteres especiales
   		if (textASCII_decode>=32 && textASCII_decode<=64) {
+        // para descrifrar parte con el tope al final por eso 64
+        asciiCode_decode=(textASCII_decode-64-numClave_decode)%33+64;
+        textoCipher_decode+=String.fromCharCode(asciiCode_decode);
   			
-
-  		}
+      }
+      // en el rango de las mayusculas de 65 a 90
+      else if (textASCII_decode>=65 && textASCII_decode <=90) {
+        // para descifrar el tope esta en 90
+        asciiCode_decode=(textASCII_decode-90-numClave_decode)%26+90;
+        textoCipher_decode+=String.fromCharCode(asciiCode_decode);
+      }
+      // rango de minusculas 97 a 122
+      else if(textASCII_decode>=97 && textASCII_decode<122){
+        textoCipher_decode+=String.fromCharCode(asciiCode_decode);
+      }
 
 
   	}
+    return textoCipher_decode;
     /* Acá va tu código */
   }
 };
