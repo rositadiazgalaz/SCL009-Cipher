0f4d2e3ec5731f44775d8960b9b351c5964daaaa
rositadiazgalaz <rositadiazgalaz@gmail.com>
2019-04-08 12:26:35 -0400

    manejo del DOM

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// MANIPULAR DOM
+const btnCifrar=document.getElementById("cifrar-btn");
+btnCifrar.addEventListener("click", ()=>{
+	let numero=document.getElementById("clave").value;
+	let texto=document.getElementById("mensaje").value;
+	let resultado=cipher.encode(numero, texto);
+	document.getElementById("mensaje").value=resultado;
+})
+const btnDescifrar=document.getElementById("descrifrar-btn");
+btnDescifrar.addEventListener("click", ()=>{
+	let numeroD=document.getElementById("clave").value;
+	let textoD=document.getElementById("mensaje").value;
+	let resultado=cipher.decode(numeroD,textoD);
+	document.getElementById("mensaje").value=resultado;
+} )
